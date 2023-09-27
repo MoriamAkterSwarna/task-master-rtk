d06d3f971d4e131fb10a13935ed186b10f49e1fc
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2023-09-27 19:38:12 +0600

    asyncthunk added and register with firebase done

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,21 +1,26 @@
-import { useEffect, useState } from 'react';
-import loginImage from '../assets/image/login.svg';
-import { useForm, useWatch } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import loginImage from "../assets/image/login.svg";
+import { useForm, useWatch } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { createUser } from "../redux/features/user/userSlice";
+import toast from "react-hot-toast";
 
 const Signup = () => {
   const { handleSubmit, register, control } = useForm();
-  const password = useWatch({ control, name: 'password' });
-  const confirmPassword = useWatch({ control, name: 'confirmPassword' });
+  const password = useWatch({ control, name: "password" });
+  const confirmPassword = useWatch({ control, name: "confirmPassword" });
   const navigate = useNavigate();
   const [disabled, setDisabled] = useState(true);
+  const { isError, error } = useSelector((state) => state.userSlice);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (
       password !== undefined &&
-      password !== '' &&
+      password !== "" &&
       confirmPassword !== undefined &&
-      confirmPassword !== '' &&
+      confirmPassword !== "" &&
       password === confirmPassword
     ) {
       setDisabled(false);
@@ -24,8 +29,18 @@ const Signup = () => {
     }
   }, [password, confirmPassword]);
 
+  useEffect(() => {
+    toast.error(error);
+  }, [isError, error]);
   const onSubmit = ({ name, email, password }) => {
     // Email Password signup
+    dispatch(
+      createUser({
+        email,
+        password,
+        name,
+      })
+    );
     console.log(name, email, password);
   };
 
@@ -48,7 +63,7 @@ const Signup = () => {
                 type="text"
                 id="name"
                 className="w-full rounded-md"
-                {...register('name')}
+                {...register("name")}
               />
             </div>
             <div className="flex flex-col items-start">
@@ -57,7 +72,7 @@ const Signup = () => {
                 type="email"
                 id="email"
                 className="w-full rounded-md"
-                {...register('email')}
+                {...register("email")}
               />
             </div>
             <div className="flex flex-col items-start">
@@ -66,7 +81,7 @@ const Signup = () => {
                 type="password"
                 id="password"
                 className="w-full rounded-md"
-                {...register('password')}
+                {...register("password")}
               />
             </div>
             <div className="flex flex-col items-start">
@@ -75,25 +90,23 @@ const Signup = () => {
                 type="password"
                 id="confirm-password"
                 className="w-full rounded-md"
-                {...register('confirmPassword')}
+                {...register("confirmPassword")}
               />
             </div>
             <div className="!mt-8 ">
               <button
                 type="submit"
                 className="btn btn-primary w-full disabled:bg-gray-300 disabled:cursor-not-allowed"
-                disabled={disabled}
-              >
+                disabled={disabled}>
                 Sign up
               </button>
             </div>
             <div>
               <p>
-                Already have an account?{' '}
+                Already have an account?{" "}
                 <span
                   className="text-primary hover:underline cursor-pointer"
-                  onClick={() => navigate('/login')}
-                >
+                  onClick={() => navigate("/login")}>
                   Login
                 </span>
               </p>
@@ -101,8 +114,7 @@ const Signup = () => {
             <button
               type="button"
               className="btn btn-primary w-full"
-              onClick={handleGoogleLogin}
-            >
+              onClick={handleGoogleLogin}>
               Login with Google
             </button>
           </form>
